24808a40856b410f236d3ab86d4d230db691bb47
ne0x86 <ne0x86@users.noreply.github.com>
2018-11-30 13:14:29 +0100

    Agregar archivos de proyecto.

---
+++ Buscaminas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Buscaminas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
